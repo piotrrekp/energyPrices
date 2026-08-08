e9644188ef0b16fbe9d025b6eba1d17fd3cb6128
Piotr Pluciński <p.plucinski@wb.com.pl>
2026-08-08 06:24:15 +0200

    main zwraca tabelke z cenami
    Change-Id: I9af54afe0fc39ef16831eaba26eaf747457defb6

--- app/main.cc
+++ app/main.cc
@@ -1,6 +1,20 @@
+#include "httpClient.h"
+#include "tgeParser.h"
 #include <iostream>
+
 
 int main() {
 	std::cout << "działam!" << std::endl;
+
+	httpClient client("https://tge.pl/energia-elektryczna-rdn");
+	auto page = client.getPage();
+	TgeParser parser;
+	auto table = parser.parseEnergyPricesTable(page);
+	std::cout << table << std::endl;
+
+
+
+
+	std::cout << "już nie..." << std::endl;
 	return 0;
 }
